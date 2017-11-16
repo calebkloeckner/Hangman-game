c978d5ce3e82f31cd3917a5350fce2b9b5313b0f
calebkloeckner <calebkloeckner@gmail.com>
2017-11-16 09:54:24 -0700

    first progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,93 @@
-//Words for game
-var words = ["football", "baseball", "movies", "colorado", "skydive"];
+console.log("working");
+// // ============ VARIABLES ========= //
+// //Words for game
+// var hangman = ["football", "baseball", "movies", "colorado", "skydive"];
 
-var word = words[Math.floor(Math.random() * words.length)];
+// var countdown = 10;
+// var wins = 0;
+// var losses = 0;
+// document.querySelector("#wins").HTML = wins;
+// document.querySelector("#losses").HTML = losses;
 
-// Answer
-var answerArray = [];
-for (var i = 0; i < word.length; i++); {
-    answerArray[i] = "_";
+// // ============= CUSTOM FUNCTIONS ================== //
+// function startGame() {
+
+// // random word
+// var word = hangman[Math.floor(Math.random() * hangman.length)];
+// // Answer
+// console.log(word);
+// var answerArray = []; 
+// for (var i = 0; i < word.length; i++) { // takes lenght of word
+//     answerArray.push("_"); // pushes underscore into letters of word
+// }
+
+// document.querySelector("#click-begin").HTML = answerArray;
+// }
+// startGame();
+// // console.log(answerArray);
+// // =================== EVENT LISENTERS ========================== //
+// // Game loop
+// document.onkeyup = function(event) { // listening to key being pushed
+// console.log(event);
+// var guess = event.key; 
+// var inWord = false; // runs through word to match 
+
+// for (var j = 0; j < word.length; j++) { // matches guess in to word
+//     if (word[j] === guess) {
+//         answerArray[j] = guess; 
+//         inWord = true;
+//     }
+// };
+
+// if (inWord = false) { // if guess is false, countdown goes down one
+//     countdown--;
+//     // print guess[j];
+//     document.querySelector("#countdown").HTML = countdown;
+// }
+
+// if (word.length = true) {
+//     alert("You win");
+// } else if (countdown === 0) {
+//     alert("You lose!");
+// };
+    
+// };
+
+// // document.querySelector("#game").innerHTML = html;
+
+
+var hangman = ["baseball", "america", "what", "beach"];
+
+var guesses = [];
+var wins = 0;
+var losses = 0;
+var missed;
+var countdown = 10;
+
+function startGame() {
+console.log("startGame");
+
+var word = hangman[Math.floor(Math.random() * hangman.length)];
+console.log(word);
+document.querySelector("#click-begin").innerHTML = ("Good luck");
+var answerArray = []; 
+    for (var i = 0; i < word.length; i++) { // takes lenght of word
+answerArray.push("_");  
+document.querySelector("#game-board").innerHTML = answerArray;
 }
 
-// Variable for letters remaining
-var remainingLetters = word.length;
+}
+document.onkeyup = function(event) { // listening to key being pushed
+var drawnWord = "";
 
-// Game loop
-
-while (remainingLetters > 0) {
-    var guess = prompt("Guess a letter.");
-    if (guess === null) {
-        break;
-
-    } else if (guess.length !== 1) {
-        alert("Please enter single letter.");
-    } else {
-        for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
-                answerArray[j] = guess;
-                remainingLetters--;
-            }
-        }
+function draw() {
+    drawnWord = "";
+for (var j = 0; j < word.length; j++) { // matches guess in to word
+     if (word[j] === guess) {
+        answerArray[j] = guess; 
+        drawnWord = true;
     }
+    drawnWord.includes(guess);
+};
+}
 }
